4aa4ac151204091b278846b0b2c8ac6dba33c2f5
Sophie <soph1155@stud.kea.dk>
2024-10-21 12:05:24 +0200

    import af værktøjer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import { animate } from "motion";
 import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
