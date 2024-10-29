c2cecc4690656f65dba295b6edca40000dc6aa3e
Sophie <soph1155@stud.kea.dk>
2024-10-29 16:10:05 +0100

    styling Forskel.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			keyframes: {
-				bobble: {
-					'0%, 100%': { transform: 'translateY(0) scale(1)' },
-					'50%': { transform: 'translateY(-10px) scale(1.1)' },
-				},
-			},
-			animation: {
-				bobble: 'bobble 1.5s ease-in-out infinite',
-			},
-		},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: "#FF6F3C", // Orange farve som du ønsker
+      },
+      keyframes: {
+        bobble: {
+          "0%, 100%": { transform: "translateY(0) scale(1)" },
+          "50%": { transform: "translateY(-10px) scale(1.1)" },
+        },
+      },
+      animation: {
+        bobble: "bobble 1.5s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
