024cb2a2a683dbc2ff23df0841bc94addf000d36
Sophie <soph1155@stud.kea.dk>
2024-10-30 17:11:48 +0100

    baggrund

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
     extend: {
       colors: {
         orange: "#FF6F3C", // Orange farve som du ønsker
+        darkblue: "#013755", //mørkeblå
+        lightblue: "#e6f6ff" /* Lyseblå */,
+        white: "#ffffff" /* Hvid */,
       },
       keyframes: {
         bobble: {
@@ -16,21 +19,16 @@ export default {
         bobble: "bobble 1.5s ease-in-out infinite",
       },
       width: {
-        '70p': '80%',
-        '22p': '35%',
-        '8p': '20%',
+        "70p": "80%",
+        "22p": "35%",
+        "8p": "20%",
       },
       height: {
-        '70p': '80%',
-        '22p': '35%',
-        '8p': '20%',
+        "70p": "80%",
+        "22p": "35%",
+        "8p": "20%",
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/typography"), 
-    require("@tailwindcss/forms"), 
-    require("@tailwindcss/aspect-ratio"), 
-    require("@tailwindcss/container-queries")
-  ],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
 };
